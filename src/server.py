d7c5acb658a64c40948f02e914e5861d3ad4f50a
Subhasish Paul <51708663+subhasishpaul@users.noreply.github.com>
2022-02-12 13:45:49 +0530

    Update server.py

--- src/server.py
+++ src/server.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World! Automation testing."
+    return "Hello World! Automation testing. Double quote to used inside stages"
